402c6cb365aba4337a5c46ad1a9ba206c359db32
Alfred Melch <dev@melch.pro>
2019-12-07 09:18:38 +0100

    Include python 3.7 in tests

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     keywords=['docker', 'secret', 'envvar', 'config'],
     py_modules=['get_docker_secret'],
